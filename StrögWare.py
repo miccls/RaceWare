2538de047df6579577c3a27fba9568d6c4fdccef
miccls <martincsvardsjo@gmail.com>
2020-11-18 12:28:53 +0100

    Huvudklassen för programmet.

---
+++ StrögWare.py
@@ -0,0 +1,17 @@
+# Huvudklass för StrögWare, mjukvaran för storströgarnas racedator.
+import pygame
+
+class StrögWare:
+	'''Huvudklass som innehåller programmets högsta logik'''
+
+	def __init__(self):
+		# Fixa skärmen och allt sånt soft.
+
+		_init_track()
+
+	def run(self):
+		
+
+	def _init_track(self):
+		'''Hjälpmetod som ställer in allt efter
+		vald bana'''
